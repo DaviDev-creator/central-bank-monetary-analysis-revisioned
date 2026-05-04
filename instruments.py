c729679c6bea60341f358500ac0fb962a7f68191
DaviDev-creator <davidelicari875@gmail.com>
2026-05-04 18:23:29 +0200

    Update financial instruments: add all major central banks

--- instruments.py
+++ instruments.py
@@ -10,5 +10,15 @@ financial_instruments = {
     "GOLD": [
         "https://www.worldgoldcouncil.org",
         "https://www.federalreserve.gov"
-    ]
+    ],
+    "FED": ["https://www.federalreserve.gov"],
+    "BCE": ["https://www.ecb.europa.eu"],
+    "BoE": ["https://www.bankofengland.co.uk"],
+    "BoJ": ["https://www.boj.or.jp/en"],
+    "SNB": ["https://www.snb.ch/en"],
+    "BoC": ["https://www.bankofcanada.ca"],
+    "RBA": ["https://www.rba.gov.au"],
+    "RBNZ": ["https://www.rbnz.govt.nz"],
+    "NorgesBank": ["https://www.norges-bank.no/en"],
+    "Riksbank": ["https://www.riksbank.se/en-gb"]
 }
